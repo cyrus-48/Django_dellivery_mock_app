96efb69535941da0fd25bdae1aadbbd206a19993
cyrus mwendwa <78921046+cyrus-48@users.noreply.github.com>
2022-08-16 21:36:18 +0300

    models based forms

--- deliveryapp/forms.py
+++ deliveryapp/forms.py
@@ -1,5 +1,17 @@
-from  django import  forms
-class PizzaForm(forms.Form):
-    topping1 = forms.CharField(label='topping 1', max_length=100)
-    topping2 = forms.CharField(label='topping 2', max_length=100)
-    size = forms.ChoiceField(label='size' , choices=[('small','small'),('medium' , 'medium'),('large','large') ])
+from django import forms
+from .models import *
+
+
+# class PizzaForm(forms.Form):
+#     topping1 = forms.CharField(label='topping 1', max_length=100)
+#     topping2 = forms.CharField(label='topping 2', max_length=100)
+#     size = forms.ChoiceField(label='size' , choices=[('small','small'),('medium' , 'medium'),('large','large') ])
+class PizzaForm(forms.ModelForm):
+    class Meta:
+        model = Pizza
+        fields = ['topping1', 'topping2', 'size']
+        labels = {'topping1': 'Topping 1', 'topping2': 'Topping 2'}
+
+
+class MultiplePizzasForm(forms.Form):
+    number = forms.IntegerField(min_value=2, max_value=8)
--- deliveryapp/views.py
+++ deliveryapp/views.py
@@ -1,8 +1,8 @@
+from django.forms import formset_factory
 from django.shortcuts import render
 
-
 # Create your views here.
-from deliveryapp.forms import PizzaForm
+from deliveryapp.forms import PizzaForm, MultiplePizzasForm
 
 
 def home(request):
@@ -10,20 +10,44 @@ def home(request):
 
 
 def order(request):
+    multiple_form = MultiplePizzasForm()
     if request.method == "POST":
 
         filled_form = PizzaForm(request.POST)
+
         if filled_form.is_valid():
-            note = "Thank you for ordering %s %s and %s pizza on the way"  %(filled_form.cleaned_data['size'],
-            filled_form.cleaned_data['topping1'],
-            filled_form.cleaned_data['topping2'],)
+            note = "Thank you for ordering %s %s and %s pizza on the way" % (filled_form.cleaned_data['size'],
+                                                                             filled_form.cleaned_data['topping1'],
+                                                                             filled_form.cleaned_data['topping2'],)
             new_form = PizzaForm()
             context = {
-                'pizzaForm':new_form,
-                'note':note,
+                'pizzaForm': new_form,
+                'note': note,
+                'multiple_form': multiple_form,
             }
             return render(request, 'pizza/order.html', context)
 
     else:
         form = PizzaForm()
-        return render(request, 'pizza/order.html', {'pizzaForm':form})
+        return render(request, 'pizza/order.html', {'pizzaForm': form, 'multiple_form': multiple_form, })
+
+
+def pizzas(request):
+    number_of_pizzas = 2
+    filled_multiple_pizza_form = MultiplePizzasForm(request.GET)
+    if filled_multiple_pizza_form.is_valid():
+        number_of_pizzas = filled_multiple_pizza_form.cleaned_data['number']
+    pizza_form_set = formset_factory(PizzaForm, extra=number_of_pizzas)
+    formset = pizza_form_set()
+    if request.method == 'POST':
+        filled_formset = pizza_form_set(request.POST)
+        if filled_formset.is_valid():
+            for form in filled_formset:
+                print(form.cleaned_data['topping1'])
+            note = "pizzas have been ordered"
+        else:
+            note = " pizzas not ordered , please try again "
+        return render(request, 'pizza/pizza.html', {'note': note, 'formset': formset})
+
+    else:
+        return render(request, 'pizza/pizza.html', {'formset': formset})
